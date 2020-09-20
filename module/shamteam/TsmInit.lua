abd8f615ef6dc2065524e0a94802660062351ad2
cngc4e <cngc4e@gmail.com>
2020-09-20 15:19:50 +0800

    shamteam: Fix room name matching
    A recent big update changed the way room names are parsed and now allows
    modules to be initialised using a space delimiter in the room name.
    
    i.e. "#shamteam AoB" would previously fail to initialise, but would now work.

--- module/shamteam/TsmInit.lua
+++ module/shamteam/TsmInit.lua
@@ -1,13 +1,13 @@
 local IsOfficialRoom = function(name)
     local normalised_matches = {
-        "^%w-%-#(.-)$",  -- public room, in form of xx-#module(0args)?
-        "^*#(.-)$"  -- private room, in form of *#module(0args)?
+        "^%w-%-#(.-)$",  -- public room, in form of xx-#module([%s|%d]args)?
+        "^*#(.-)$"  -- private room, in form of *#module([%s|%d]args)?
     }
     for i = 1, #normalised_matches do
         local x = name:match(normalised_matches[i])
         if x then name = x break end
     end
-    return name and (name == MODULE_ROOMNAME or name:find("^"..MODULE_ROOMNAME.."%d+.-$"))
+    return name and (name == MODULE_ROOMNAME or name:find("^"..MODULE_ROOMNAME.."[ %d].-$"))
 end
 
 init_ext = function()
